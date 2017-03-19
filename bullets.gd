7275150aba0a4f811c6544679d974fa231c9a4f1
Gunny Waffle <davidafoxworthy@gmail.com>
2017-03-18 23:24:28 -0400

    modular funcs for bullet behaviour

--- bullets.gd
+++ bullets.gd
@@ -18,13 +18,26 @@ func _draw():
 	for b in bullets:
 		draw_texture(texture, b.pos + tofs)
 
+# edit me to change where bullets spawn
+func start_pos(index):
+	var pos = Vector2(get_viewport_rect().size * Vector2(randf()*2.0, randf()))
+	pos.x += get_viewport_rect().size.x
+	return pos
+
+# edit me to change how the bullets move
+func move(b, delta):
+	var width = get_viewport_rect().size.x*2.0
+	b.pos.x -= b.speed * delta
+	if (b.pos.x < -30):
+		b.pos.x += width
+
 
 func _ready():
 	texture = preload("res://icon.png")
 	var radius = texture.get_size()*0.5
 	
 	shape = Physics2DServer.shape_create(Physics2DServer.SHAPE_CIRCLE)
-	Physics2DServer.shape_set_data(shape, radius) # Radius
+	Physics2DServer.shape_set_data(shape, radius)
 	
 	for i in range(BULLET_COUNT):
 		var b = Bullet.new()
@@ -33,8 +46,7 @@ func _ready():
 		Physics2DServer.body_set_space(b.body, get_world_2d().get_space())
 		Physics2DServer.body_add_shape(b.body, shape)
 		
-		b.pos = Vector2(get_viewport_rect().size * Vector2(randf()*2.0, randf())) # Twice as long
-		b.pos.x += get_viewport_rect().size.x # Start outside
+		b.pos = start_pos(i)
 		var mat = Matrix32()
 		mat.o = b.pos
 		Physics2DServer.body_set_state(b.body, Physics2DServer.BODY_STATE_TRANSFORM, mat)
@@ -45,12 +57,9 @@ func _ready():
 
 
 func _process(delta):
-	var width = get_viewport_rect().size.x*2.0
 	var mat = Matrix32()
 	for b in bullets:
-		b.pos.x -= b.speed*delta
-		if (b.pos.x < -30):
-			b.pos.x += width
+		move(b, delta)
 		mat.o = b.pos
 		
 		Physics2DServer.body_set_state(b.body, Physics2DServer.BODY_STATE_TRANSFORM, mat)
