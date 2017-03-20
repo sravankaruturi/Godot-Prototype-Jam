3edda33e11cd10d72954d2b3fb15614432593185
TheEvilBanana <hpaul2094@gmail.com>
2017-03-20 18:20:50 -0400

    Added game logic, bullet spawn and game over logic
    Bullets now spawn when player score points with a little increase in
    speed
    Players have 3 lives and game quits after player has lost 3 lives

--- master.gd
+++ master.gd
@@ -1,20 +1,19 @@
-const NUM_BULLETS = 10
+const NUM_BULLETS = 8
 var hit_bullets = []
-var scoreblue = 0;
-var scorered = 0;
+var score = 0;
+var dead = 3;
+var bspeed = 100;
 
 func _ready():
 	randomize()
-	
 	var bullets = get_node("bullets")
 	var bullet_scene = load("res://bullet.tscn")
 	for i in range(NUM_BULLETS):
 		var bullet = bullet_scene.instance()
-		bullet.id = i
+		bullet.id = i 
 		bullet.speed = 100
-		
-		
 		bullets.add_child(bullet)
+	
 	
 	set_process(true)
 
@@ -25,19 +24,33 @@ func _process(delta):
 	for b in hit_bullets:
 		var dir = (b.get_pos() - p_pos).normalized()
 		var check = dir.dot(forward)
-		if (check > 0 and b.is_in_group("bullet A")):
-			print("dead A") # replace with logic for getting hit
-			b.free()
-			scorered = scorered + 1
-			print(scorered)
-			get_node("scoretextred").set_text(String(scorered))
+		if (check > 0):
+			if(b.is_in_group("bullet A")):
+				b.free()
+				score = score + 1
+				get_node("score").set_text(String(score))
+				spawn_bullets()
 			
-		elif (check <= 0 and b.is_in_group("bullet B")):
-			print("dead B") # replace with logic for getting hit
-			b.free()
-			scoreblue = scoreblue + 1
-			print(scoreblue)
-			get_node("scoretextblue").set_text(String(scoreblue))
+			elif(b.is_in_group("bullet B")):
+				b.free()
+				dead = dead - 1 
+				get_node("dead").set_text(String(dead))
+				check_game_over()
+				
+		elif (check <= 0):
+			#print("dead B") # replace with logic for getting hit
+			if(b.is_in_group("bullet B")):
+				b.free()
+				score = score + 1
+				get_node("score").set_text(String(score))
+				spawn_bullets()
+			
+			elif(b.is_in_group("bullet A")):
+				b.free()
+				dead = dead - 1 
+				get_node("dead").set_text(String(dead))
+				check_game_over()
+	
 
 
 func _on_player_area_enter( area ):
@@ -46,3 +59,18 @@ func _on_player_area_enter( area ):
 	
 func _on_player_area_exit( area ):
 	hit_bullets.erase(area)
+
+func spawn_bullets():
+	var bullets = get_node("bullets")
+	var bullet_scene = load("res://bullet.tscn")
+	var j = randi()%10
+	var bullet = bullet_scene.instance()
+	bspeed = bspeed + 10
+	bullet.id = j
+	bullet.speed = bspeed
+	bullets.add_child(bullet)
+	
+func check_game_over():
+	#Add game over screen here and remove the quit game logic
+	if(dead == 0):
+		get_tree().quit()
